4c5707eb9a2cd6f18024d78881f5eb97033699e0
humayun.rashid@dreambroker.fi <humayun.rashid@dreambroker.fi>
2020-03-17 18:06:49 +0200

    updated

--- routes/routes.js
+++ routes/routes.js
@@ -3,38 +3,28 @@ const router = express.Router()
 const fetch = require('node-fetch');
 const url = 'https://covid19.mathdro.id/api'
 
-
-router.get('/', function(req,res){
-    res.render('country')
-})
-
 router.post('/', getGlobalCoronaDataByCountry, function(req,res){
-    res.send(res.response)
+    res.render('country', { data: res.response })
 })
 
-router.get('/corona/global',getGlobalCoronaData, function(req,res){
-    res.send(res.response)
+router.get('/',getGlobalCoronaData, function(req,res){
+    res.render('home', { data: res.response })
 })
 
 router.get('/corona/:country',getGlobalCoronaDataByCountry, async function(req,res){
     
     try{
-        const country = await req.params.country
-        const response = await res.response
-        if (response == null){
+        const data = await res.response
+        res.render('country', { data: data }) 
+        
+        if (data == null){
             return res.status(404).json({message:err.message})
         }
-        else {
-            const confirm = response.confirmed['value']
-            const recovered = response.recovered['value']
-            const deaths = response.deaths['value']
-            const date = response.lastUpdate
-            const data ={"Country":country, "Date":date, "Confirmed": confirm, "Recovered":recovered, "Deaths":deaths}
-            res.send(data)
-        }   
+                
     } catch (err){
         return res.status(500).json({message:err.message})
     }
+    
 })
 
 router.get('/countries', async function(req,res){
@@ -49,29 +39,32 @@ router.get('/countries', async function(req,res){
 async function getGlobalCoronaData (req,res,next){
     let response
     let json
+    let country = "Global"
     try{
         response = await fetch(url)
         json = await response.json()
+        console.log(json['confirmed'])
 
         if (json == null) {
             return res.status(404).json({message:err.message.value})
         }
 
+
     } catch (err) {
         return res.status(500).json({message:err.message})
     }
-
-    res.response = json
+    const data = { "Total":json.value, "Confirmed":json.confirmed.value,"Recovered":json.recovered.value, "Deaths":json.deaths.value, "Date": json.lastUpdate, "Country": "Global" }
+    res.response = data
     next()
 }
 
 async function getGlobalCoronaDataByCountry (req,res,next){
-    let countryUrl
+    // let countryUrl
     let Country
     if (req.params.country && !req.body.country) Country = await req.params.country
     if (!req.params.country && req.body.country) Country = await req.body.country
     console.log (Country)
-    countryUrl = url + '/countries/'+ Country
+    const countryUrl = url + '/countries/'+ Country
 
     // if (req.params.country && !req.body.country) countryUrl = url + '/countries/'+ Country
     // if (!req.params.country && req.body.country) countryUrl = url + '/countries/'+ Country
@@ -89,14 +82,22 @@ async function getGlobalCoronaDataByCountry (req,res,next){
     } catch (err) {
         return res.status(500).json({message:err.message})
     }
-    const confirm = json.confirmed['value']
-    const recovered = json.recovered['value']
-    const deaths = json.deaths['value']
-    const date = json.lastUpdate
-    const data ={"Country":Country, "Date":date, "Confirmed": confirm, "Recovered":recovered, "Deaths":deaths}
-    res.response = data
+    // const data = { "Total":json.value, "Confirmed":json.confirmed.value,"Recovered":json.recovered.value, "Deaths":json.deaths.value, "Date": json.lastUpdate,"Country":"Global" }
+
+    res.response = await getData(json,Country)
+    console.log(res.response)
 
     next()
+}
+
+async function getData(json,country){
+    const confirm = await json.confirmed['value']
+    const recovered = await json.recovered['value']
+    const deaths = await json.deaths['value']
+    const date = await json.lastUpdate
+    const data ={ "Country":country,"Date":date, "Confirmed": confirm, "Recovered":recovered, "Deaths":deaths}
+    return data
+
 }
 
 module.exports = router

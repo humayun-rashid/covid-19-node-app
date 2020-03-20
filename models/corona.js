61ba68356f892df20be31d8f489ea04bb50731d1
humayun.rashid@dreambroker.fi <humayun.rashid@dreambroker.fi>
2020-03-20 11:52:35 +0200

    mongoose model is created

---
+++ models/corona.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const coronaSchema = new mongoose.Schema (
+{
+    country:{
+        type:String,
+        required: true
+    },
+    confirmed:{
+        type:String,
+        required: true
+    },
+    recovered:{
+        type:String,
+        required: true
+    },
+    deaths:{
+        type:String,
+        required: true
+    },
+    lastUpdate:{
+        type:String,
+        required: true
+    },
+    createdAt:{
+        type:Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model ('Corona',coronaSchema)
